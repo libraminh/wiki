4a67a908f3c55dac15555d10734fd02ce1dd5c9a
Minh Le <minh.le@construct.sg>
2019-07-01 17:52:23 +0700

    [master] update content

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   title: 'Minh Le',
   description: 'Wikipedia',
-  base: '/docs',
+  base: '/',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
